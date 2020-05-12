4077fdcbc367191f8afbeb9abd2a23be61996ed5
Callum Hutchinson <callumhutchie@hotmail.co.uk>
2020-05-12 01:07:17 +0100

    Adding initial project

---
+++ gw-skills/build/precache-manifest.2c4a14be81e042195c8c356f228d91ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d659df229ff1d1595ea6f1d56234bf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c18cd36f4d8fce86a59",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e7f042efbe7e415c3ba",
+    "url": "/static/js/2.84ce2370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84ce2370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c18cd36f4d8fce86a59",
+    "url": "/static/js/main.0af5b62c.chunk.js"
+  },
+  {
+    "revision": "bf466efa57eee7caf890",
+    "url": "/static/js/runtime-main.2d79266d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ gw-skills/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" >
+        <p>
+          Template Code
+          <br></br>
+          <input id="template"></input>
+        </p>
+
+        <div id="skills">
+          <div id="skill-1" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+          <div id="skill-2" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+          <div id="skill-3" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+          <div id="skill-4" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+          <div id="skill-5" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+          <div id="skill-6" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+          <div id="skill-7" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+          <div id="skill-8" class="skill">
+            <img class="skill-img" src="https://wiki.guildwars.com/images/b/b9/Vow_of_Strength.jpg" alt=""></img>
+          </div>
+        </div>
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
